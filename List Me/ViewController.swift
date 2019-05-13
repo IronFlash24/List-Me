e812f7624c20721dc5b7ee6aaae1df008f9821d2
Mayura Mathure <mayura@hjl07423.local>
2019-05-13 23:13:08 +0530

    Covert to MVC design Pattern

--- List Me/ViewController.swift
+++ List Me/ViewController.swift
@@ -10,40 +10,67 @@ import UIKit
 
 class ListMeViewController: UITableViewController {
     
-    var itemArray = ["classes","test","back home"]
+    var itemArray = [item]()
     let defaluts = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaluts.array(forKey: "TodoListArray") as? [String]{
+        let newItem1 = item()
+        newItem1.title = "Find Mike"
+        itemArray.append(newItem1)
+        
+        let newItem2 = item()
+        newItem2.title = "Buy Eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = item()
+        newItem3.title = "Destroy Demogorgan"
+        itemArray.append(newItem3)
+        
+        if let items = defaluts.array(forKey: "TodoListArray") as? [item]{
             itemArray = items
         }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+// THE ABOVE LINE IS THE SAME AS THE BOTTOM CODE
+        
+//        if itemArray[indexPath.row].done == false{
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true)
+
+
 }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -53,7 +80,10 @@ class ListMeViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             print("success")
             //print(textField.text)
-            self.itemArray.append(textField.text!)
+            let newItem = item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaluts.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ List Me/data model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  List Me
+//
+//  Created by Mayura Mathure on 13/05/19.
+//  Copyright © 2019 Mayura Mathure. All rights reserved.
+//
+
+import Foundation
+
+class item {
+    var title : String = ""
+    var done : Bool = false
+}
